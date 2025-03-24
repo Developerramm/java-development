1b42ec60136325865bc15f60da1dd62e6da7b6e1
ram kumar <rr697144@gmail.com>
2025-03-24 08:24:24 +0530

    hello world program

---
+++ basicjava/src/variableanddatatype/Main.java
@@ -0,0 +1,8 @@
+package variableanddatatype;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Ram Kumar");
+        System.out.println("Noida");
+    }
+}
