85d4a6246f9a15234c5a1f66c4dafb1b37edbe9f
ram kumar <rr697144@gmail.com>
2025-03-24 16:24:35 +0530

    data type in java

--- basicjava/src/variableanddatatype/Main.java
+++ basicjava/src/variableanddatatype/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Ram Kumar");
         System.out.println("Noida");
+        System.out.println("Maniyari");
     }
 }
---
+++ basicjava/src/variableanddatatype/Variable.java
@@ -0,0 +1,33 @@
+package variableanddatatype;
+
+public class Variable {
+    public static void main(String[] args) {
+        System.out.println("Variable in java ");
+        boolean isProgramer = true;
+        System.out.println(isProgramer);
+        isProgramer = false;
+        System.out.println(isProgramer);
+
+        byte number1 = 120;
+        System.out.println(number1);
+
+        short number2 = 24343;
+        System.out.println(number2);
+
+        int number3 = 3243424;
+        System.out.println(number3);
+
+        double number4 = 43242.3423;
+        System.out.println(number4);
+
+        float number5 = 43242.342f;
+        System.out.println(number5);
+
+        long number6 = 4342353423224342l;
+        System.out.println(number6);
+
+        char ch = 'A';
+        System.out.println(ch);
+    }
+
+}
