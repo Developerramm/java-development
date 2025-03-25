2468492e1e5494d066736f1b43271c49675f7e4c
ram kumar <rr697144@gmail.com>
2025-03-25 08:43:43 +0530

    logical operator

---
+++ basicjava/src/operator/LogicalOperator.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class LogicalOperator {
+    public static void main(String[] args) {
+        System.out.println("Logical operator in java ");
+        System.out.println(10 > 5 && 20 < 50);
+        System.out.println(10 > 20);
+        System.out.println(100 > 200 || 100 < 200);
+        System.out.println(!true);
+        System.out.println(!false);
+    }
+}
