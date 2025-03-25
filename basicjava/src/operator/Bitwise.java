c45aa32f1b34961eaeac3b845adba70fa473ffca
ram kumar <rr697144@gmail.com>
2025-03-25 13:21:38 +0530

    bitwise operator in java

---
+++ basicjava/src/operator/Bitwise.java
@@ -0,0 +1,15 @@
+package operator;
+
+public class Bitwise {
+    public static void main(String[] args) {
+        System.out.println("Bitwise operator in java ");
+        System.out.println(5 & 5);
+        System.out.println(5 | 5);
+        System.out.println(5 | 8);
+        System.out.println(5 & 8);
+        System.out.println(~9);
+        System.out.println(5 << 2);
+        System.out.println(5 >> 2);
+    }
+
+}
