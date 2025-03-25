474eb9185d2806db92e746b5dd32a35e68dc6171
ram kumar <rr697144@gmail.com>
2025-03-25 08:07:18 +0530

    arithmetic operator in java

---
+++ basicjava/src/operator/Arithmetic.java
@@ -0,0 +1,20 @@
+package operator;
+
+public class Arithmetic {
+    public static void main(String[] args) {
+        System.out.println("Arithmetic operator in java ");
+        System.out.println(7 / 2);
+        System.out.println(7 % 3);
+
+        int number1 = 10;
+        System.out.println(number1++);
+        System.out.println(number1);
+        int number2 = 20;
+        System.out.println(number1 + number2);
+        System.out.println(number1 - number2);
+        System.out.println(number1 * number2);
+        System.out.println(number1 / number2);
+
+    }
+
+}
