1c6fb8799bffcb355f11b01056db6df3f52501ae
ram kumar <rr697144@gmail.com>
2025-03-25 14:50:23 +0530

    do whle loop in java

---
+++ basicjava/src/loops/DoWhileLoop.java
@@ -0,0 +1,15 @@
+package loops;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        System.out.println("Do while loop in java ");
+        int count = 1;
+
+        do {
+            System.out.print(count + " ");
+            count++;
+        } while (count <= 100);
+
+    }
+
+}
