e036c49e73fddaf42c7d05bf51fbd86ed496e680
ram kumar <rr697144@gmail.com>
2025-03-25 14:43:46 +0530

    sum of first 100 natural number using for loop

---
+++ basicjava/src/loops/ForLoop.java
@@ -0,0 +1,15 @@
+package loops;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        System.out.println("For loop in java");
+
+        int sum = 0;
+        for(int i = 1; i<= 100; i++){
+            sum += i;
+        }
+        System.out.println(sum);
+
+    }
+
+}
